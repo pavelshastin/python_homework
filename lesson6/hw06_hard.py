60ae9d56214ffb247bcf64f150c3db82be2f710a
Павел Шастин <C:\Users\P.Shastin\AppData\Roaming\The Bat!>
2019-01-28 14:14:33 +0300

    lesson6 hard DataDase class

--- lesson6/hw06_hard.py
+++ lesson6/hw06_hard.py
@@ -13,11 +13,10 @@
 
 import os
 import re
+init_db_dir = os.getcwd()
 
 class DataBase:
-    """
 
-    """
     def __init__(self, db_name):
         cur_dir = os.getcwd()
 
@@ -30,32 +29,42 @@ class DataBase:
         self.db_name = db_name
         self.headers = None
         self.f = None
+        self.head_line = False
 
 
 
     def connect(self, table):
         if self.f is None:
             self.f = open(table, "r", encoding="UTF-8")
-
+            self._setHeaders(self.f)
             return self
+
+        if self.f.name == table:
+            return self
+
         raise ValueError("To establish new connection you need to close previous one.")
 
-    def getHeaders(self):
+    def _setHeaders(self, f_iter):
         if self.headers is None:
-            self.f.__iter__()
-            line = self.f.readline()
+            f_iter.seek(0, 0)
+            line = f_iter.readline()
 
             self.headers = tuple(map(lambda x: x.strip(), re.split(r" {3,}", line)))
 
-        return self.headers
+
+    def getHeaders(self):
+        return self.headers if self.headers else None
+
 
     def getAll(self):
-        self.f.__iter__()
-        headers = self.getHeaders()
-        return list(map(lambda line: dict(zip(headers, line.split())), self.f.readlines()))
+        self.f.seek(0, 0)
+        self.f.readline() #to skip the first header line
 
-    def nextLine(self):
-        pass
+        res = list(map(lambda line: dict(zip(self.headers, line.split())), self.f.readlines()))
+
+        self.f.seek(0, 0) #to return the caret to the beginning
+
+        return res
 
     def close(self):
         self.f.close()
@@ -63,11 +72,35 @@ class DataBase:
         self.headers = None
 
 
+    def __iter__(self):
+        self.f.seek(0, 0)
+        self.f.readline()  # to skip the first header line
+
+        if self.headers is None:
+            self.getHeaders()
+
+        for line in self.f:
+            yield dict(zip(self.headers, line.split()))
+
+        self.f.seek(0, 0)
+        self.head_line = False
+
+
+    def __next__(self):
+        #to skip the first header line
+        if self.head_line is False:
+            self.f.readline()
+            self.head_line = True
+        res = dict(zip(self.headers, self.f.readline().split()))
+
+        return res if res else False
+
 
 db = DataBase("data")
 
-print(os.getcwd())
+
 print(db.connect("hours_of").getAll())
 db.close()
+print(db.connect("workers").getHeaders())
 print(db.connect("workers").getAll())
 
